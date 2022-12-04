9dbbde5a870d64fd3fcf962c48b9fd39d1f06364
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-04 22:03:16 +0900

    querySelectorAll 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var list = document.querySelectorAll("p > li");
+  var list = document.querySelectorAll(".test");
   for (var item of list) {
     item.style.color = "green";
     item.style.fontSize = "55px";
