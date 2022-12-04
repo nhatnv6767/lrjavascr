1b13caa098357f5341024936437979386378362a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-04 21:42:52 +0900

    get elements by tag name 1

--- js/main.js
+++ js/main.js
@@ -1,25 +1,7 @@
-function getEle(id) {
-  return document.getElementById(id)
-}
-
-function batDen() {
-  var den = getEle("theDen");
-  den.src = "./img/pic_bulbon.gif"
-}
-
-function tatDen() {
-  var den = getEle("theDen");
-  den.src = "./img/pic_bulboff.gif"
-}
-
-
-function dangNhap() {
-  var userName = getEle("username");
-  var pass = getEle("password");
-  var sectionThongBao = getEle("sectionThongBao")
-  var thePThongBao = getEle("thePThongBao")
-  if (userName.value == 'username' && pass.value == 'password') {
-    sectionThongBao.style.backgroundColor = "green"
-    thePThongBao.innerHTML = "Login Successfully"
-  }
+function myFunction() {
+  var list = document.getElementsByTagName("ul")
+  var ulDauTien = list[0];
+  var listLi = list[0].getElementsByTagName("li");
+  var liDauTien = listLi[0];
+  liDauTien.innerHTML = "CHANGED"
 }
