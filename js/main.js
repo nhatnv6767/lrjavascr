9f6648ea3567eff964607d42bf78f620999a4127
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-04 13:32:11 +0900

    change src 2

--- js/main.js
+++ js/main.js
@@ -2,37 +2,12 @@ function getEle(id) {
   return document.getElementById(id)
 }
 
-
-function thayDoiNoiDung(id, noiDung) {
-  var theP = getEle(id)
-  theP.innerHTML = noiDung
+function batDen() {
+  var den = getEle("theDen");
+  den.src = "./img/pic_bulbon.gif"
 }
 
-
-function anThe(id) {
-  var the = getEle(id)
-  the.style.display = 'none'
-}
-
-function thayDoiStyle(id) {
-  var the = getEle(id);
-  the.style.fontSize = "30px"
-  the.style.color = "red"
-}
-
-function changeDiv(id) {
-  var the = getEle(id);
-  the.style.backgroundColor = "green"
-  the.style.width = "100px";
-}
-
-function getStyleInfo(id) {
-  var the = getEle(id);
-  return the.style.width;
-}
-
-function nhanNut1() {
-
-  changeDiv("theDiv")
-  thayDoiNoiDung("content1", getStyleInfo("theDiv"))
+function tatDen() {
+  var den = getEle("theDen");
+  den.src = "./img/pic_bulboff.gif"
 }
