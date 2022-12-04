58828ffbc861b88cbac613abb633a60401e4c72e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-04 22:00:47 +0900

    querySelector 1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 function myFunction() {
-  var list = document.getElementsByTagName("ul")
-  var ulDauTien = list[0];
-  var listLi = list[0].getElementsByTagName("li");
-  var liDauTien = listLi[0];
-  liDauTien.innerHTML = "CHANGED"
-  liDauTien.style.color = "red";
-  liDauTien.style.fontSize = "50px"
-
-  // change by class
-  var list = document.getElementsByClassName("test");
-  list[0].style.color = "red";
-  list[1].style.color = "red";
+  var theP = document.querySelector("p");
+  theP.innerHTML = "CHANGED";
+  theP.style.color = "green"
 }
