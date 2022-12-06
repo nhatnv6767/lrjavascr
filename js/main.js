19b9d524fd2a162f37939cccacede5c6d4d3a376
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-06 21:12:43 +0900

    tinh cuoc uber 6

--- js/main.js
+++ js/main.js
@@ -21,3 +21,23 @@ function kiemTraLoaiXe() {
     return "uberBlack"
   }
 }
+
+function tinhTienCho(thoiGianCho, giaCho) {
+  var tienCho = 0;
+  if (thoiGianCho >= 3) {
+    tienCho = Math.round(thoiGianCho / 3.0) * giaCho;
+  }
+  return tienCho;
+}
+
+
+function tinhTien(soKm, thoiGianCho, arrayPrice, giaCho) {
+  var tienCho = tinhTienCho(thoiGianCho, giaCho);
+  if (soKm <= 1) {
+    return arrayPrice[0] + tienCho
+  } else if (soKm > 1 && soKm <= 20) {
+    return arrayPrice[0] + (soKm - 1) * arrayPrice[1] + tienCho;
+  } else if (soKm > 20) {
+    return arrayPrice[0] + 19 * arrayPrice[1] * (soKm - 20) * arrayPrice[2] + tienCho;
+  }
+}
