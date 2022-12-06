b71ad3a788457bff716992aca7918863646d2aa9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-06 23:16:02 +0900

    validation 4.1

--- js/validate.js
+++ js/validate.js
@@ -1,5 +1,9 @@
 var pattern = /cyberlearn/ig;
 
 document.querySelector("#btnVerify").onclick = function () {
-  alert("VERI")
+  // lay thong tin nguoi dung nhap vao tu input
+  var text = document.querySelector("#text").value;
+
+  // kiem tra thong tin hop le voi pattern hay khong
+  console.log(pattern.test(text))
 }
