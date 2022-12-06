84ea8557cee85fae9c17d54cabe838e76eb467a5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-06 23:40:25 +0900

    validation 6

--- js/validate.js
+++ js/validate.js
@@ -3,12 +3,17 @@ var pattern = /cyberlearn/ig;
 document.querySelector("#btnVerify").onclick = function () {
   // lay thong tin nguoi dung nhap vao tu input
   var txtInput = document.querySelector("#text");
+  var spanTB = document.querySelector("#tb")
   var text = txtInput.value;
 
   // kiem tra thong tin hop le voi pattern hay khong
   if (pattern.test(text)) {
     txtInput.style.borderBottom = "3px solid green"
+    spanTB.innerHTML = "Dữ liệu hợp lệ"
+    spanTB.className = "alert alert-success"
   } else {
     txtInput.style.borderBottom = "3px solid red"
+    spanTB.innerHTML = "Dữ liệu không hợp lệ!!!"
+    spanTB.className = "alert alert-danger"
   }
 }
