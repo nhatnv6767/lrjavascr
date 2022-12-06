8cb3a92ddcaf2eb75e3cb4805ef3c0ca8535eea0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-06 23:38:04 +0900

    validation 5

--- js/validate.js
+++ js/validate.js
@@ -2,8 +2,13 @@ var pattern = /cyberlearn/ig;
 
 document.querySelector("#btnVerify").onclick = function () {
   // lay thong tin nguoi dung nhap vao tu input
-  var text = document.querySelector("#text").value;
+  var txtInput = document.querySelector("#text");
+  var text = txtInput.value;
 
   // kiem tra thong tin hop le voi pattern hay khong
-  console.log(pattern.test(text))
+  if (pattern.test(text)) {
+    txtInput.style.borderBottom = "3px solid green"
+  } else {
+    txtInput.style.borderBottom = "3px solid red"
+  }
 }
