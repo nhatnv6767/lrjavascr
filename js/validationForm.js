8d18e1e7bbbfde84efd3542da777ad8a070d4aa1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-07 22:31:14 +0900

    validationForm 5

--- js/validationForm.js
+++ js/validationForm.js
@@ -1,10 +1,16 @@
 var checkValidation = function () {
   var firstName = document.getElementById("firstName").value;
   if (firstName === "") {
-    document.getElementById("error_firstName").innerHTML = "Họ tên không được bỏ trống"
+    document.getElementById("error_firstName").innerHTML = "Họ không được bỏ trống"
+  }
+
+  var lastName = document.getElementById("lastName").value;
+  if (lastName === "") {
+    document.getElementById("error_lastName").innerHTML = "Tên không được bỏ trống"
   }
 }
 
-document.getElementById('firstName').onfocusout = checkValidation;
+document.getElementById('firstName').onblur = checkValidation;
+document.getElementById('lastName').onblur = checkValidation;
 
 document.getElementById("btnDangKy").onclick = checkValidation
