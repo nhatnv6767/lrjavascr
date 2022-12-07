34db4b604d6add69edab19b42ab2cfa9aa5d1ceb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-07 22:24:50 +0900

    validationForm 2

---
+++ js/validationForm.js
@@ -0,0 +1,8 @@
+var checkValidation = function () {
+  var firstName = document.getElementById("firstName").value;
+  if (firstName === "") {
+    document.getElementById("error_firstName").innerHTML = "Họ tên không được bỏ trống"
+  }
+}
+
+document.getElementById("btnDangKy").onclick = checkValidation()
