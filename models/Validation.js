2c6de7170a65f1eb93f89608e58ffcf6f7b64963
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-08 22:43:34 +0900

    OOP 16

--- models/Validation.js
+++ models/Validation.js
@@ -1,6 +1,13 @@
 var Validation = function () {
-    this.kiemTraRong = function (value) {
-        return value.trim() !== "";
+    this.kiemTraRong = function (value, name, selectorError) {
+        if (value.trim() === "") {
+            document.querySelector(selectorError).innerHTML = name + " không được bỏ trống"
+            document.querySelector(selectorError).style.display = "block";
+            return false
+        }
+        document.querySelector(selectorError).innerHTML = ""
+        document.querySelector(selectorError).style.display = "none";
+        return true;
 
     }
 }
