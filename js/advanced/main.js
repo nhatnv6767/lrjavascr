fd9045f48dba0cafb79584e921e3a45bb0fd8f49
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-08 23:28:55 +0900

    Manage Student 6

--- js/advanced/main.js
+++ js/advanced/main.js
@@ -1,6 +1,14 @@
 var danhSachSinhVien = new DanhSachSinhVien();
+var validate = new Validation();
 
 function ThemSinhVien() {
+    // lay du lieu tu nguoi dung
+    var masv = document.getElementById("masv").value;
+    var hoten = document.getElementById("hoten").value;
+    var cmnd = document.getElementById("cmnd").value;
+    var email = document.getElementById("email").value;
+    var sdt = document.getElementById("sdt").value;
+
     // kiem tra validation
-    
+    validate.KiemTraRong(masv)
 }
