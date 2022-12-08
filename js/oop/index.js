94cb6e2f7418e1c8334d5b725aea3d5a3642c5e5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-08 21:31:14 +0900

    OOP 5

--- js/oop/index.js
+++ js/oop/index.js
@@ -1,46 +1,46 @@
 var xuLyXacNhan = function () {
-  var maSinhVien = document.querySelector("#maSinhVien").value;
-  var tenSinhVien = document.querySelector("#tenSinhVien").value;
-  var loaiSinhVien = document.querySelector("#loaiSinhVien").value;
-  var diemToan = document.querySelector("#diemToan").value;
-  var diemLy = document.querySelector("#diemLy").value;
-  var diemHoa = document.querySelector("#diemHoa").value;
-  var diemRenLuyen = document.querySelector("#diemRenLuyen").value;
-  var diemTrungBinh = tinhDiemTrungBinh(diemToan, diemLy, diemHoa)
-  var xepLoaiSinhVien = xepLoai(diemTrungBinh, diemRenLuyen)
-  // xu ly in ket qua
+    var maSinhVien = document.querySelector("#maSinhVien").value;
+    var tenSinhVien = document.querySelector("#tenSinhVien").value;
+    var loaiSinhVien = document.querySelector("#loaiSinhVien").value;
+    var diemToan = document.querySelector("#diemToan").value;
+    var diemLy = document.querySelector("#diemLy").value;
+    var diemHoa = document.querySelector("#diemHoa").value;
+    var diemRenLuyen = document.querySelector("#diemRenLuyen").value;
+    var diemTrungBinh = tinhDiemTrungBinh(diemToan, diemLy, diemHoa)
+    var xepLoaiSinhVien = xepLoai(diemTrungBinh, diemRenLuyen)
+    // xu ly in ket qua
 
-  document.querySelector("#txtTenSinhVien").innerHTML = tenSinhVien;
-  document.querySelector("#txtMaSinhVien").innerHTML = maSinhVien;
-  document.querySelector("#txtLoaiSinhVien").innerHTML = loaiSinhVien;
-  document.querySelector("#txtDiemTrungBinh").innerHTML = diemTrungBinh.toFixed(2);
-  document.querySelector("#txtXepLoai").innerHTML = xepLoaiSinhVien;
+    document.querySelector("#txtTenSinhVien").innerHTML = tenSinhVien;
+    document.querySelector("#txtMaSinhVien").innerHTML = maSinhVien;
+    document.querySelector("#txtLoaiSinhVien").innerHTML = loaiSinhVien;
+    document.querySelector("#txtDiemTrungBinh").innerHTML = diemTrungBinh.toFixed(2);
+    document.querySelector("#txtXepLoai").innerHTML = xepLoaiSinhVien;
 }
 
 
 var tinhDiemTrungBinh = function (diemToan, diemLy, diemHoa) {
-  var dtb = (Number(diemToan) + Number(diemLy) + Number(diemHoa)) / 3;
-  return dtb;
+    var dtb = (Number(diemToan) + Number(diemLy) + Number(diemHoa)) / 3;
+    return dtb;
 }
 
 var xepLoai = function (diemTrungBinh, diemRenLuyen) {
-  if (diemRenLuyen < 5) {
-    return "Yếu"
-  } else {
-    if (diemTrungBinh < 5) {
-      return "Yếu"
-    } else if (diemTrungBinh >= 5 && diemTrungBinh < 6.5) {
-      return "Trung Bình";
-    } else if (diemTrungBinh >= 6.5 && diemTrungBinh < 8) {
-      return "Khá"
-    } else if (diemTrungBinh >= 8 && diemTrungBinh < 9) {
-      return "Giỏi"
-    } else if (diemTrungBinh >= 9 && diemTrungBinh <= 10) {
-      return "Xuất sắc"
+    if (diemRenLuyen < 5) {
+        return "Yếu"
     } else {
-      return "Không hợp lệ"
+        if (diemTrungBinh < 5) {
+            return "Yếu"
+        } else if (diemTrungBinh >= 5 && diemTrungBinh < 6.5) {
+            return "Trung Bình";
+        } else if (diemTrungBinh >= 6.5 && diemTrungBinh < 8) {
+            return "Khá"
+        } else if (diemTrungBinh >= 8 && diemTrungBinh < 9) {
+            return "Giỏi"
+        } else if (diemTrungBinh >= 9 && diemTrungBinh <= 10) {
+            return "Xuất sắc"
+        } else {
+            return "Không hợp lệ"
+        }
     }
-  }
 }
 
 document.querySelector("#btnXacNhan").onclick = xuLyXacNhan;
