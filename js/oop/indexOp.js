f13a9f5b6012433b2cc1f25e6c1426488eb32de0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-08 22:26:06 +0900

    OOP 11

--- js/oop/indexOp.js
+++ js/oop/indexOp.js
@@ -1,15 +1,12 @@
-var sinhVien = { // scope instance
-    maSV: 1,
-    tenSV: "Nguyen Thanh Than",
-    loaiSinhVien: "Kho Khan",
-    diemToan: 1,
-    diemLy: 5,
-    diemHoa: 7,
-    tinhDiemTrungBinh: function () {
-        var dtb = (Number(this.diemToan) + Number(this.diemLy) + Number(this.diemHoa)) / 3
-        return dtb.toFixed(2)
-    }
+var sinhVien = new SinhVien();
+var xuLyXacNhan = function () {
+    sinhVien.maSV = document.querySelector("#maSinhVien").value;
+    sinhVien.tenSV = document.querySelector("#tenSinhVien").value;
+    sinhVien.loaiSinhVien = document.querySelector("#loaiSinhVien").value;
+    sinhVien.diemToan = document.querySelector("#diemToan").value;
+    sinhVien.diemLy = document.querySelector("#diemLy").value;
+    sinhVien.diemHoa = document.querySelector("#diemHoa").value;
+    sinhVien.diemRenLuyen = document.querySelector("#diemRenLuyen").value;
+    console.log(sinhVien)
 }
-
-var sinhVien2 = new SinhVien()
-console.log(sinhVien2)
+document.querySelector("#btnXacNhan").onclick = xuLyXacNhan()
