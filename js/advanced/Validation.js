3bd380b568ffe0c356d4ae0590036bd5d747a19c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-09 07:08:45 +0900

    Manage Student 11

--- js/advanced/Validation.js
+++ js/advanced/Validation.js
@@ -5,4 +5,17 @@ function Validation() {
         }
         return false
     }
+
+
+    this.KiemTraEmail = function (value) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(value.toLowerCase());
+    }
+    this.KiemTraSoDT = function (value) {
+        var re = /^\d+$/;
+        if (re.test(value) && value.length >= 10) {
+            return true;
+        }
+        return false;
+    }
 }
