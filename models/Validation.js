745f00c5a953ffd16b03d53005be830466d3b61d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-08 22:51:27 +0900

    OOP 18

--- models/Validation.js
+++ models/Validation.js
@@ -8,6 +8,28 @@ var Validation = function () {
         document.querySelector(selectorError).innerHTML = ""
         document.querySelector(selectorError).style.display = "none";
         return true;
+    }
+    // kiem tra tat ca la so
+    this.kiemTraTatCaLaSo = function (value, name, selectorError) {
+        var regexNumber = /^[0-9]+$/;
+        if (regexNumber.test(value)) {
+            document.querySelector(selectorError).innerHTML = ""
+            document.querySelector(selectorError).style.display = "none";
+            return true
+        }
+        document.querySelector(selectorError).innerHTML = name + " tất cả phải là số"
+        document.querySelector(selectorError).style.display = "block";
+    }
+    // kiem tra do dai chuoi
 
+    this.kiemTraDoDaiChuoi = function (value, name, selectorError, minLength, maxLength) {
+        if (value.trim().length < minLength || value.trim().length > maxLength) {
+            document.querySelector(selectorError).innerHTML = name + ' từ ' + minLength + ' đến ' + maxLength + ' ký tự';
+            document.querySelector(selectorError).style.display = 'block';
+            return false;
+        }
+        document.querySelector(selectorError).innerHTML = '';
+        document.querySelector(selectorError).style.display = 'none';
+        return true;
     }
 }
