15ec1afa5e41b2b08ca7249b14d253e3f3ec576a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-10 20:42:53 +0900

    Manage Student 21 delete func

--- js/advanced/DanhSachSinhVien.js
+++ js/advanced/DanhSachSinhVien.js
@@ -4,6 +4,14 @@ function DanhSachSinhVien() {
         this.DSSV.push(svThem)
     }
     this.XoaSinhVien = function (listSVXoa) {
+        for (var i = 0; i < listSVXoa.length; i++) {
+            for (var j = 0; j < this.DSSV.length; j++) {
+                var sinhvien = this.DSSV[j]
+                if (listSVXoa[i] === sinhvien.MaSV) {
+                    this.DSSV.splice(j, 1);
+                }
+            }
+        }
     }
     this.SuaSinhVien = function (svCapNhat) {
     }
